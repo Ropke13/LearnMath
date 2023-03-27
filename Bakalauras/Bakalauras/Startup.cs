e34feae8d00b14c2f8e0aebc4a35bc0071836998
Ropke13 <79408886+Ropke13@users.noreply.github.com>
2023-03-27 22:33:08 +0300

    connected database

--- Bakalauras/Bakalauras/Startup.cs
+++ Bakalauras/Bakalauras/Startup.cs
@@ -27,6 +27,7 @@ namespace Bakalauras
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -49,6 +50,8 @@ namespace Bakalauras
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSession();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
